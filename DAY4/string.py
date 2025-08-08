b243be6b09908f60a80e79014555bc14af7afc26
Kirin <huyak2009@gmail.com>
2025-08-08 11:00:37 +0700

    Initial commit

---
+++ DAY4/string.py
@@ -0,0 +1,35 @@
+#1
+text = ' '.join(['Thirty', 'Days', 'Of', 'Python'])
+print(text)
+
+#2
+text2 = ' '.join(['Coding', 'For', 'All'])
+print(text2)
+
+#3
+company = "Coding For All"
+
+#4
+print(company)
+
+#5
+print(len(company))
+
+#6
+print(company.upper())
+
+#7
+print(company.lower())
+
+#8
+print(company.capitalize())  
+print(company.title())      
+print(company.swapcase())   
+
+#9
+print(company[7:])  
+
+#10
+print('Coding' in company)          
+print(company.find('Coding'))     
+print(company.index('Coding'))
